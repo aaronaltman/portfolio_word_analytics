3959e47cd31a963b3c5bb3932660c8f3b013c733
aka <aaronkaltman@gmail.com>
2023-12-29 13:17:50 -0500

    getting better

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -4,20 +4,20 @@ export default function Analytics({ text }: { text: string }) {
   const faceBook = 2200 - text.length;
   const twitterCount = 280 - text.length;
   return (
-    <div className="flex flex-wrap bg-slate-900 p-4 rounded-xl">
-      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+    <div className="flex flex-wrap bg-slate-900 p-4 rounded-xl shadow-sm">
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white/10 m-2 p-4 rounded-xl">
         <p className="text-3xl"> {charCount} </p>
         <p className="text-center">Character Count</p>
       </div>
-      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white/10 m-2 p-4 rounded-xl">
         <p className="text-3xl"> {wordCount} </p>
         <p>Word Count</p>
       </div>
-      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white/10 m-2 p-4 rounded-xl">
         <p className="text-3xl"> {faceBook} </p>
         <p>Facebook</p>
       </div>
-      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white/10 m-2 p-4 rounded-xl">
         <p className="text-3xl"> {twitterCount} </p>
         <p>Twitter</p>
       </div>

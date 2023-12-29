226315cdd5be1cbe10fdaed9833249c36417f03e
aka <aaronkaltman@gmail.com>
2023-12-29 13:16:06 -0500

    this is pretty good

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,18 +1,26 @@
-import React from "react";
-
 export default function Analytics({ text }: { text: string }) {
   const wordCount = text.split(/\s+/).filter((word) => word !== "").length;
   const charCount = text.length;
   const faceBook = 2200 - text.length;
+  const twitterCount = 280 - text.length;
   return (
-    <div className="flex flex-wrap justify-between gap-10 py-2 px-20">
-      <p className="flex flex-col border border-black/10 w-1/2">
-        {" "}
-        {charCount} Chars
-      </p>
-      <p>{wordCount}</p>
-      <p>{faceBook}</p>
-      <p>Twitter</p>
+    <div className="flex flex-wrap bg-slate-900 p-4 rounded-xl">
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+        <p className="text-3xl"> {charCount} </p>
+        <p className="text-center">Character Count</p>
+      </div>
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+        <p className="text-3xl"> {wordCount} </p>
+        <p>Word Count</p>
+      </div>
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+        <p className="text-3xl"> {faceBook} </p>
+        <p>Facebook</p>
+      </div>
+      <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white m-2 p-4">
+        <p className="text-3xl"> {twitterCount} </p>
+        <p>Twitter</p>
+      </div>
     </div>
   );
 }

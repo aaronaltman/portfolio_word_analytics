e52282fb37d60bdbbf39f51c70765ab02a1ab516
aka <aaronkaltman@gmail.com>
2023-12-29 13:25:23 -0500

    better

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mt-auto p-2 bg-slate-900">
+    <footer className="px-4 py-10 bg-slate-900">
       <div className="flex justify-between max-w-6xl text-white mx-auto">
         <small>Footer</small>
         <small>Copyright</small>

4c44a7d422a82b2e13d2ad07ade9e5213f880b81
aka <aaronkaltman@gmail.com>
2023-12-29 13:35:21 -0500

    almost there

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,17 +1,17 @@
 export default function Analytics({ text }: { text: string }) {
   const wordCount = text.split(/\s+/).filter((word) => word !== "").length;
   const charCount = text.length;
-  const faceBook = 2200 - text.length;
-  const twitterCount = 280 - text.length;
+  const faceBook = 2200 - charCount;
+  const twitterCount = 280 - charCount;
   return (
     <div className="flex flex-wrap bg-slate-900 p-4 rounded-xl shadow-sm">
       <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white/10 m-2 p-4 rounded-xl">
         <p className="text-3xl"> {charCount} </p>
-        <p className="text-center">Character Count</p>
+        <p className="text-center">Characters</p>
       </div>
       <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white/10 m-2 p-4 rounded-xl">
         <p className="text-3xl"> {wordCount} </p>
-        <p>Word Count</p>
+        <p>Words</p>
       </div>
       <div className="flex-1 min-w-1/2 flex flex-col text-white justify-center items-center border border-white/10 m-2 p-4 rounded-xl">
         <p className="text-3xl"> {faceBook} </p>
--- src/components/background.tsx
+++ src/components/background.tsx
@@ -5,7 +5,7 @@ export default function Background() {
   return (
     <main className="bg-slate-700 min-h-screen">
       <section className="bg-slate-100 py-20  max-w-8xl mx-auto ">
-        <h1 className="text-center text-6xl">Word Analytics</h1>
+        <h1 className="text-center text-6xl font-bold">Word Analytics</h1>
       </section>
       <MainCard />
     </main>
